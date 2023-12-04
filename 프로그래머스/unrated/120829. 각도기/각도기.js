85b9f00871be592913162e4522d8043f623627f2
YenaKwak <145232967+YenaKwak@users.noreply.github.com>
2023-12-04 15:55:12 +0100

    [level unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,27 @@
+function solution(angle) {
+if (angle > 0 && angle < 90){
+    return 1;
+} else if (angle === 90){
+    return 2;
+} else if (angle > 90 && angle < 180){
+    return 3;    
+} else if (angle === 180){
+    return 4;    
+}
+
+    
+const angle1 = 70;
+const angle2 = 91;
+const angle3 = 181;
+
+console.log(angle1);
+console.log(angle2);
+console.log(angle3);
+
+
+    
+    
+    
+    
+    
+}
