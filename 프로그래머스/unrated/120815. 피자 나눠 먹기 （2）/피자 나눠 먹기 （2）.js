5a6f93577129b4cf653caa8e8e80159f93663022
YenaKwak <145232967+YenaKwak@users.noreply.github.com>
2024-01-06 22:11:53 +0100

    Update 피자 나눠 먹기 （2）.js

--- 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -12,3 +12,17 @@ const solution = (n) => {
 
     return piece / 6
 }
+
+
+
+
+
+const solution = (n) => {
+    let piece = 6;
+
+    for (; piece % n !== 0; piece += 6) {
+        // 반복문 내에서는 특별한 처리가 필요 없음
+    }
+// 루프의 초기식(initialization) 부분은 세미콜론 ;으로 시작합니다. 이 부분에서는 이미 초기화된 piece 변수를 사용하며, 반복 조건에 해당하는 표현식이 포함되어 있습니다.
+    return piece / 6;
+};
