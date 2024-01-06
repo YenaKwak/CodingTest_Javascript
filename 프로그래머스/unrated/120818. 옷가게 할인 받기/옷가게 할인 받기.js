7bfa09c33a57201f84ad6d35e263fec6bfe1a522
YenaKwak <145232967+YenaKwak@users.noreply.github.com>
2024-01-06 21:30:19 +0100

    [level unrated] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,25 @@
+// A 5% discount is given for purchases over 100,000 won, a 10% discount for purchases over 300,000 won, and a 20% discount for purchases over 500,000 won.
+// Given the price of the clothes purchased, complete the solution function to return the amount to be paid.
+
+function solution(price){
+    let discountRate = 0;
+    
+// 가장 큰 할인율부터 작은 할인율 순서대로 조건을 확인해야 합니다.
+    if (price >= 500000) {
+        discountRate = 0.2;
+    } else if (price >= 300000) {
+        discountRate = 0.1;
+    } else if (price >= 100000) {
+        discountRate = 0.05;
+    }
+    
+  let discountAmount = price * discountRate;
+    let discountedPrice = price - discountAmount;
+    return Math.floor(discountedPrice);
+}
+
+
+    let clothesPrice = 580000;
+    let result = solution(clothesPrice);
+    console.log(result);
+    
