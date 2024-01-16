479178a6e6689e4f307e28a919cdb04e4abc6b2e
YenaKwak <145232967+YenaKwak@users.noreply.github.com>
2024-01-16 21:40:24 +0100

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -0,0 +1,10 @@
+// function solution(rny_string){
+//     return rny_string.split('m').join('rn');
+    
+// }
+
+
+function solution(rny_string){
+    return rny_string.replace(/m/g, 'rn');
+                             
+}
