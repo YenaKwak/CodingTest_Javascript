95228bfff3efd16ad0a709b933b40b6f7e60a4c2
YenaKwak <145232967+YenaKwak@users.noreply.github.com>
2024-01-24 18:14:44 +0100

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,23 +1,4 @@
 function solution(array, n) {
-    let closestNumber = array[0];
-
-    for (let i = 1; i < array.length; i++) {
-        const currentDiff = Math.abs(array[i] - n);
-        const closestDiff = Math.abs(closestNumber - n);
-
-        if (currentDiff < closestDiff || (currentDiff === closestDiff && array[i] < closestNumber)) {
-            closestNumber = array[i];
-        }
-    }
-
-    return closestNumber;
-}
-
-
-
-
-function solution(array, n) {
-    array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
-
+    array.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
     return array[0];
 }
